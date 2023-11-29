3a6af7b3604c5bcf21fb4b450103c9e05ba4e9e5
raviraj3214 <raviraj32140@gmail.com>
2023-11-29 12:53:19 +0530

    added .sh

---
+++ .scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo "Deployment started..."
+
+# Pull the latest version of the app
+git pull origin main
+echo "New changes copied to server !"
+
+echo "change directory"
+cd client
+
+echo "Installing Dependencies..."
+pnpm install --yes
+
+echo "Creating Production Build..."
+# For ReactJS VueJS and Nuxt JS
+pnpm run build
+
+# For NextJS
+# npm run export
+
+echo "Deployment Finished!"
