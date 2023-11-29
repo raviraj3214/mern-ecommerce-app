2831df88fab16ded74607f5f24282d761c28cf2b
raviraj3214 <raviraj32140@gmail.com>
2023-11-29 23:05:09 +0530

    vhbj

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -12,21 +12,7 @@ git fetch
 git merge origin/main -m "Merge remote-tracking branch into main"
 
 echo "New changes copied to server !"
-pwd
-echo "change directory"
-cd client
-pwd
 
-
-echo "Installing Dependencies..."
-npm install
-
-echo "Creating Production Build..."
-# For ReactJS VueJS and Nuxt JS
-npm run build
-
-# npm run export
-cd ..
 
 echo "sdcvbdfgdgfcfghbcvdfcgvhbjhbjnmkjnfghv"
 echo "Deployment Finished!"
