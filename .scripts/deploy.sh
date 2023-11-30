0e7f7239976143f227c06aff6ff29dd5da9af762
raviraj3214 <raviraj32140@gmail.com>
2023-11-30 11:43:41 +0530

    sdfghj

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -14,5 +14,5 @@ git merge origin/main -m "Merge remote-tracking branch into main"
 echo "New changes copied to server !"
 
 
-echo "sdcvbdfgdgfcdfghsdsdfghfghmkjnfghv"
+echo "sdcvbdfgdgfcdfghsdsdfghfsdfghghmkjnfghv"
 echo "Deployment Finished!"
